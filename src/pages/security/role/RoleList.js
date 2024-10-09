8174fe14f1ef643e60108611ec8559b1ac83744f
asilbek <ak.fayzullayev@gmail.com>
2024-10-09 17:48:09 +0500

    Role

--- src/pages/security/role/RoleList.js
+++ src/pages/security/role/RoleList.js
@@ -1,41 +1,97 @@
-import { Table } from "antd";
+import { Button, Card, Popconfirm, Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import dayjs from "dayjs";
+import { getRoleList } from "../../../store/actions/role/role";
+import { useEffect } from "react";
+import UserService from "../../../services/UserService";
 
 const RoleList = () => {
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { roleList, isLoading } = useSelector((state) => state.role);
+
+  useEffect(() => {
+    dispatch(getRoleList());
+  }, [dispatch]);
+
+  const ActionComponent = ({
+    item,
+    // handleDeletePermission,
+    // handleOpenModal,
+  }) => {
+    return (
+      <div>
+        <button
+          // onClick={() => handleOpenModal(item)}
+          title="Edit"
+          className="btn btn-sm btn-outline-success me-1"
+        >
+          <i className="bi bi-pencil-square" />
+        </button>
+        <Popconfirm
+          title="Are you sure to Delete"
+          okText="Yes"
+          cancelText="No"
+          // onConfirm={() => handleDeletePermission(item.id)}
+        >
+          <button title="Delete" className="btn btn-sm btn-outline-danger me-1">
+            <i className="bi bi-trash" />
+          </button>
+        </Popconfirm>
+      </div>
+    );
+  };
 
   const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "Created At",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt) => dayjs(createdAt).format("MM-DD-YYYY hh:mm"),
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: (_, item) => (
+        <ActionComponent
+          item={item}
+          // handleDeletePermission={handleDeletePermission}
+          // handleOpenModal={handleOpenModal}
+        />
+      ),
     },
   ];
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />
+      <Card
+        extra={
+          UserService.hasPermission("ADD_ROLE") && (
+            <Button type="primary">Create Role</Button>
+          )
+        }
+      >
+        <Table
+          size="small"
+          loading={isLoading}
+          dataSource={roleList}
+          columns={columns}
+        />
+      </Card>
     </div>
   );
 };
