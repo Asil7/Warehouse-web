f3b184fe69ef266ffd4e47ddd0533abff80b9f71
asilbek <ak.fayzullayev@gmail.com>
2024-10-22 18:07:51 +0500

    Order product

--- src/pages/order/OrderProductList.js
+++ src/pages/order/OrderProductList.js
@@ -1,6 +1,24 @@
 import { Card } from "antd";
 
 const OrderProductList = () => {
+  const columns = [
+    {
+      title: "Product",
+      dataIndex: "product",
+      key: "product",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+    },
+  ];
+
   return (
     <div>
       <Card></Card>
