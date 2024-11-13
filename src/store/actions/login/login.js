c7bd4107ecbe640a5cde2e1a733638277b7e4204
asilbek <asil.biohazard@gmail.com>
2024-11-13 22:27:15 +0500

    Log

--- src/store/actions/login/login.js
+++ src/store/actions/login/login.js
@@ -5,12 +5,12 @@ import api from "../../../api";
 
 export const login = createAsyncThunk("login", async (data) => {
   try {
-    // const res = await axios.post(`http://localhost:8080/api/auth/login`, data);
+    const res = await axios.post(`http://localhost:8080/api/auth/login`, data);
     // const res = await axios.post(`https://192.168.1.62:8080/api/auth/login`, data);
-    const res = await axios.post(
-      `https://192.168.100.179:8080/api/auth/login`,
-      data
-    );
+    // const res = await axios.post(
+    //   `https://192.168.100.179:8080/api/auth/login`,
+    //   data
+    // );
     // const res = await axios.post(
     //   `https://192.168.7.48:8080/api/auth/login`,
     //   data
