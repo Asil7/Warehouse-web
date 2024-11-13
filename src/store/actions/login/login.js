566edeeb2ab0af3327103b7db42ecdd104572cff
asilbek <ak.fayzullayev@gmail.com>
2024-11-13 17:32:39 +0500

    Firebase

--- src/store/actions/login/login.js
+++ src/store/actions/login/login.js
@@ -5,10 +5,12 @@ import api from "../../../api";
 
 export const login = createAsyncThunk("login", async (data) => {
   try {
-    const res = await axios.post(
-      `https://192.168.7.48:8080/api/auth/login`,
-      data
-    ); //`https://192.168.1.62:8080/api/auth/login`,  //`https://192.168.7.48:8080/api/auth/login`,
+    const res = await axios.post(`https://localhost:8080/api/auth/login`, data);
+    // const res = await axios.post(`https://192.168.1.62:8080/api/auth/login`, data);
+    // const res = await axios.post(
+    //   `https://192.168.7.48:8080/api/auth/login`,
+    //   data
+    // );
     return res;
   } catch (e) {
     return e;
