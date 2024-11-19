ae01b35e4480d015fa5d6676c085afe995587463
asilbek <ak.fayzullayev@gmail.com>
2024-11-19 16:01:47 +0500

    Login

--- src/store/actions/login/login.js
+++ src/store/actions/login/login.js
@@ -3,25 +3,46 @@ import axios from "axios";
 import api from "../../../api";
 // import { httpErrorHandler, isSuccess } from "utils/HttpUtil";
 
+// export const login = createAsyncThunk("login", async (data) => {
+//   try {
+//     const res = await axios.post(
+//       `http://167.179.118.210:8080/api/auth/login`,
+//       data
+//     );
+//     // const res = await axios.post(`http://localhost:8080/api/auth/login`, data);
+//     // const res = await axios.post(`https://192.168.1.62:8080/api/auth/login`, data);
+//     // const res = await axios.post(
+//     //   `https://192.168.100.179:8080/api/auth/login`,
+//     //   data
+//     // );
+//     // const res = await axios.post(
+//     //   `https://192.168.7.48:8080/api/auth/login`,
+//     //   data
+//     // );
+//     return res;
+//   } catch (e) {
+//     return e;
+//   }
+// });
+
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
 export const login = createAsyncThunk("login", async (data) => {
   try {
     const res = await axios.post(
       `http://167.179.118.210:8080/api/auth/login`,
-      data
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Upgrade-Insecure-Requests": "1",
+        },
+      }
     );
-    // const res = await axios.post(`http://localhost:8080/api/auth/login`, data);
-    // const res = await axios.post(`https://192.168.1.62:8080/api/auth/login`, data);
-    // const res = await axios.post(
-    //   `https://192.168.100.179:8080/api/auth/login`,
-    //   data
-    // );
-    // const res = await axios.post(
-    //   `https://192.168.7.48:8080/api/auth/login`,
-    //   data
-    // );
     return res;
   } catch (e) {
-    return e;
+    return e.response || e; // Return error response if available
   }
 });
 
