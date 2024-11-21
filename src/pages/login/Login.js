e10b99d9917e533f89c31694faf032033b584689
asilbek <ak.fayzullayev@gmail.com>
2024-11-21 13:02:58 +0500

    Login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
         message.error("Error");
       }
     } catch (error) {
-      message.error("An error occurred. Please try again." + error, 30);
+      message.error("Please try again later. Something went wrong.");
     }
   };
 
