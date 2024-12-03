54cc1c54078c4366db4d69ee954c6ddf94843404
asilbek <ak.fayzullayev@gmail.com>
2024-12-03 18:13:45 +0500

    Firebase

--- src/index.js
+++ src/index.js
@@ -24,6 +24,15 @@ const RootApp = () => {
   return <App />;
 };
 
+navigator.serviceWorker
+  .register("/firebase-messaging-sw.js")
+  .then((registration) => {
+    console.log("Service Worker registered with scope:", registration.scope);
+  })
+  .catch((error) => {
+    console.error("Service Worker registration failed:", error);
+  });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
