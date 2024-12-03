2b3611aeb314945d5bba6a2b7bdd6d05fdfa1ee1
asilbek <ak.fayzullayev@gmail.com>
2024-12-03 15:48:48 +0500

    Firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,7 +6,6 @@ importScripts(
   "https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js"
 );
 
-// Initialize Firebase app in the service worker
 firebase.initializeApp({
   apiKey: "AIzaSyCKvdHGuLjO6FZsd4nVEffKm-yIHnL2uew",
   authDomain: "warehouse-14660.firebaseapp.com",
@@ -17,33 +16,29 @@ firebase.initializeApp({
   measurementId: "G-674958JBV7",
 });
 
-// Retrieve messaging instance
 const messaging = firebase.messaging();
 
-// Handle background messages
 messaging.onBackgroundMessage((payload) => {
   console.log("Received background message: ", payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/favicon.ico", // Customize the icon
+    icon: "/favicon.ico",
     vibrate: [200, 100, 200],
-    data: { route: payload.data.route }, // Attach custom data (e.g., route)
+    data: { route: payload.data.route },
   };
 
-  // Show the notification
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Handle notification clicks
 self.addEventListener("notificationclick", (event) => {
   const route = event.notification.data?.route;
 
   console.log("Notification clicked, route: ", route);
 
-  event.notification.close(); // Close the notification
+  event.notification.close();
   if (route) {
-    event.waitUntil(clients.openWindow(route)); // Open the route
+    event.waitUntil(clients.openWindow(route));
   }
 });
